dfa225aab86c231199db25a7fa49e03c344a949e
Gil <gil4390@gmail.com>
2021-12-31 16:23:55 +0200

    client test

--- spl-net/src/main/java/bgu/spl/net/impl/BGSServer/TPCMain.java
+++ spl-net/src/main/java/bgu/spl/net/impl/BGSServer/TPCMain.java
@@ -2,15 +2,25 @@ package bgu.spl.net.impl.BGSServer;
 
 import bgu.spl.net.impl.echo.EchoProtocol;
 import bgu.spl.net.impl.echo.LineMessageEncoderDecoder;
-import bgu.spl.net.srv.BaseServer;
-import bgu.spl.net.srv.Server;
+import bgu.spl.net.srv.*;
 
 public class TPCMain {
     public static void main(String[] args) {
+        /*
         try(Server<String> server = Server.threadPerClient(7777,()->new EchoProtocol(),()->new LineMessageEncoderDecoder());){
             server.serve();
         }catch(Exception e){
             e.printStackTrace();
         }
+
+
+        Twitter twitter = new Twitter();
+
+        try(Server<Command> server = Server.threadPerClient(7777,()->new Protocol(twitter),()->new CommandEncoderDecoder());){
+            server.serve();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+*/
     }
 }
