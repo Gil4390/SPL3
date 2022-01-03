43c6f28ab9b1357d47702a910b250bc8170c2a48
Gil <gil4390@gmail.com>
2022-01-03 18:36:57 +0200

    stam

--- ConsoleApplication2/ConsoleApplication2/echoClient.cpp
+++ ConsoleApplication2/ConsoleApplication2/echoClient.cpp
@@ -9,7 +9,7 @@ using namespace std;
 */
 int main(int argc, char* argv[]) {
 
-    /*
+    /*  
         if (argc < 3) {
             std::cerr << "Usage: " << argv[0] << " host port" << std::endl << std::endl;
             return -1;
