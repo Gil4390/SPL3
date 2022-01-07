340fb877a95b5c7526c480b542debb7b62ede555
Gil Khais <gil4390@gmail.com>
2022-01-07 16:13:34 +0200

    memorty leak

--- submmision/Server/src/main/java/bgu/spl/net/impl/BGSServer/ReactorMain.java
+++ submmision/Server/src/main/java/bgu/spl/net/impl/BGSServer/ReactorMain.java
@@ -6,7 +6,10 @@ public class ReactorMain {
     public static void main(String[] args) {
         Twitter twitter = new Twitter();
 
-        try(Server<Command> server = Server.reactor(100,7777,()->new Protocol(twitter),()->new CommandEncoderDecoder());){
+        int port = Integer.parseInt(args[0]);
+        int numOfThreads = Integer.parseInt(args[1]);
+
+        try(Server<Command> server = Server.reactor(numOfThreads,port,()->new Protocol(twitter),()->new CommandEncoderDecoder());){
             server.serve();
         }catch(Exception e){
             e.printStackTrace();
